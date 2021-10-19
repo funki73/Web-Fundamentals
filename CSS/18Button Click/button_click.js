6e704dba4e7a29172a1ac5f8de83e78bce6a1059
funki73 <randy.calpe@hotmail.com>
2021-10-18 21:13:06 -0700

    1018210913

--- CSS/18Button Click/button_click.js
+++ CSS/18Button Click/button_click.js
@@ -5,9 +5,9 @@ function clickLike(){
 function clickLogin(element){
     if(element.id === "Login") {
     element.innerText = "Logout";
-} else {
-    element.innerText = "Login";
-}
+// } else {
+//     element.innerText = "Login";
+// }
 
 function clickRemove(element){
     if(element.id === "add_definition")
--- CSS/19Video Preview/script.js
+++ CSS/19Video Preview/script.js
@@ -1,1 +1,10 @@
 console.log("page loaded...");
+
+function playvid(element){
+    playvid.mute == true;
+    element.play();
+}
+function pausevid(element){
+    pausevid.mute == true;
+    element.pause();
+}
