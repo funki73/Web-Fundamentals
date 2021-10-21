32d6b43358433c9402421dd21a668e6f3e7670bd
funki73 <randy.calpe@hotmail.com>
2021-10-21 09:46:48 -0700

    1021210946

--- Algo Practice/testing.js
+++ Algo Practice/testing.js
@@ -198,4 +198,25 @@
     //         }
     //     }
     
+// complete the following function
+function flatten(arr2d) {
+    // empty to store value
+    var flat = [];
+        // iterate the outside array
+        for(var i=0; i < arr2d.length; i++){
+            // iterate the insdie array
+            for(var j=0; j < arr2d[i].length; j++){
+                // add the element back to our result array
+                flat.push(arr2d[i][j])
+                // console.log(arr2d[i][j])
+                // arr[index]
+            }
+        }
+    // print out the result
+    return flat;
+}
+    
+var result = flatten( [ [2, 5, 8], [3, 6, 1], [5, 7, 7] ] );
+console.log(result); 
 
+// we expect to get back [2, 5, 8, 3, 6, 1, 5, 7, 7]
